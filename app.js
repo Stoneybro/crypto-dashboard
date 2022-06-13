b264d2dc5a1daa26126db5ea223846c6646ce4d2
zion livingstone <zionlivingstone4@gmail.com>
2022-06-13 02:41:09 +0100

    chart.js integrated

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
+const ctx=document.getElementById('myChart').getContext("2d")
 const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
+    10,11,12,13,14,15,16,17,18,19,20,21
   ];
-
+ let gradient=ctx.createLinearGradient(0,0,0,400)
+ gradient.addColorStop(0,"rgba(116, 206, 185, 0.6)")
+ gradient.addColorStop(1,"rgba(116, 206, 185,0 )")
   const data = {
     labels: labels,
     datasets: [{
-      label: 'My First dataset',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45],
+      label: "Statistics",
+      backgroundColor: gradient,
+      borderColor: 'rgba(116, 206, 185, 1)',
+      fill:true,
+      lineTension:0.4,
+      hoverRadius:12,
+      data: [38800,38900,38750,38800,38900,38800,38850,38800,38900,38800,39050,38700]
     }]
   };
 
   const config = {
     type: 'line',
     data: data,
-    options: {}
+    options: {
+      responsive:true,
+    }
   };
 
   const myChart = new Chart(
-    document.getElementById('myChart'),
+    ctx,
     config
   );
